e3f7d8da27353383233036af8d0c0ea5a16c54d7
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-06-25 23:36:59 +0530

    created SmsReceiver.java by extends BroadcastReceiver

--- app/src/main/java/com/example/gomato/activity/login/ResetPasswordActivity.java
+++ app/src/main/java/com/example/gomato/activity/login/ResetPasswordActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.IntentSender;
 import android.os.Bundle;
 
@@ -11,21 +12,34 @@ import com.example.gomato.R;
 import com.google.android.gms.auth.api.credentials.Credential;
 import com.google.android.gms.auth.api.credentials.Credentials;
 import com.google.android.gms.auth.api.credentials.HintRequest;
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.tasks.Task;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
     private static final int CREDENTIAL_PICKER_REQUEST = 1;
+    private static final int RESOLVE_HINT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        IntentFilter intentFilter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
+        registerReceiver(smsVerificationReceiver, SmsRetriever.SEND_PERMISSION, intentFilter);
         try {
             requestHint();
         } catch (IntentSender.SendIntentException e) {
             e.printStackTrace();
         }
         startSmsUserConsent();
+    }
+
+    private void startSmsUserConsent() {
+        // Start listening for SMS User Consent broadcasts from senderPhoneNumber
+        // The Task<Void> will be successful if SmsRetriever was able to start
+        // SMS User Consent, and will error if there was an error starting.
+        Task<Void> task = SmsRetriever.getClient(context).startSmsUserConsent(8266874892);
     }
 
     private void requestHint() throws IntentSender.SendIntentException {
@@ -45,10 +59,9 @@ public class ResetPasswordActivity extends AppCompatActivity {
                 // Obtain the phone number from the result
                 if (resultCode == RESULT_OK) {
                     Credential credential = data.getParcelableExtra(Credential.EXTRA_KEY);
-                    // credential.getId();  <-- will need to process phone number string
+                    credential.getId();
                 }
-                break;
-            // ...
         }
     }
+
 }
